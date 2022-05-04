8471af1cef9cf7a20ee1ca743e4a9fcf7a1aa4b6
Nick Cao <nickcao@nichi.co>
2022-05-04 16:47:44 +0800

    merge env and build script

--- env.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-yum install -y centos-release-scl epel-release
-yum install -y devtoolset-9 cmake3 ninja-build git rh-python38
-ln -s /usr/bin/cmake3 /usr/bin/cmake
-scl enable devtoolset-9 rh-python38 ./build.sh
