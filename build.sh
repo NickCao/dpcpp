ff69ec63ee5b3e46726d422c8a06e3b0d7a8cff8
Nick Cao <nickcao@nichi.co>
2022-05-04 16:38:00 +0800

    cleanup unused files

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 python3 llvm/buildbot/configure.py --hip --hip-platform AMD -t release \
   --cmake-opt="-DSYCL_BUILD_PI_HIP_INCLUDE_DIR=$HOME/dtk-21.10/hip/include" \
   --cmake-opt="-DSYCL_BUILD_PI_HIP_HSA_INCLUDE_DIR=$HOME/dtk-21.10/hsa/include" \
